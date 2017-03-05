6b2ef0a13de5d8185a5df7bd7a78f2f4d51191d2
MisterYsLab@gmail.com <brookwood58>
2017-03-05 13:40:16 -0500

    Corrected Digital Input for Sonar

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='s2aio',
-    version='1.10',
+    version='1.11',
     packages=['s2aio'],
     install_requires=['pymata-aio>=2.7',
                       'aiohttp==0.22.4'],
